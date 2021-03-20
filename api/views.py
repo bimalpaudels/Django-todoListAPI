15ec716c917c019b0760f9d9340184fc6dff4eab
Bimal Paudel <pikachu@Bimals-MacBook-Pro.local>
2021-03-20 11:41:33 +0545

    Creating serializer and listing tasks

--- api/views.py
+++ api/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
-from django.http import JsonResponse
 
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import TaskSerializer
+from .models import Task
 
+@api_view(['GET'])
 def apiOverview(request):
-    return JsonResponse('API RESPONSE', safe=False)
+    api_urls = {
+        'List': '/task-list',
+        'Detail View': '/task-detail/<str:pk>/'
+    }
+    return Response(api_urls)
+
+
+@api_view(['GET'])
+def taskList(request):
+    tasks = Task.objects.all()
+    serializer = TaskSerializer(tasks, many=True)
+    return Response(serializer.data)
