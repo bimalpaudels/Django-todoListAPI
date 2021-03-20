620656aaf16dc73924095e1a58d2edc102a15db6
Bimal Paudel <pikachu@Bimals-MacBook-Pro.local>
2021-03-20 11:52:39 +0545

    Creating a function to get a single task detail

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns =[
-    path('', views.apiOverview, name='list-urls')
+    path('', views.apiOverview, name='list-urls'),
+    path('task-list/', views.taskList, name = 'list-task'),
+    path('task-detail/<int:pk>/', views.taskDetail, name='task-detail')
 ]
