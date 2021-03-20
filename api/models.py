ecd0959af2b00624690ca9c024e662325cd2d043
Bimal Paudel <pikachu@Bimals-MacBook-Pro.local>
2021-03-20 10:56:31 +0545

    Creating Task model and migration

--- api/models.py
+++ api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task_title = models.CharField(max_length=255, null=True)
+    task_completed = models.BooleanField(default=False)
+    task_date = models.DateTimeField("Date published")
+
