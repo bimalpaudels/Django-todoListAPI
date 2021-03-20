2c024ca8c56581815eb911931137353f9089fcc9
Bimal Paudel <pikachu@Bimals-MacBook-Pro.local>
2021-03-20 12:56:10 +0545

    Creating all the CRUD function apis

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,8 @@ from . import views
 urlpatterns =[
     path('', views.apiOverview, name='list-urls'),
     path('task-list/', views.taskList, name = 'list-task'),
-    path('task-detail/<int:pk>/', views.taskDetail, name='task-detail')
+    path('task-detail/<int:pk>/', views.taskDetail, name='task-detail'),
+    path('add-task/', views.taskAdd, name='add-task'),
+    path('update-task/<int:pk>/', views.updateTask, name='update-task'),
+    path('delete-task/<int:pk>/', views.deleteTask, name='delete-task')
 ]
--- api/views.py
+++ api/views.py
@@ -26,3 +26,30 @@ def taskDetail(request, pk):
     task = Task.objects.get(id=pk)
     serializer = TaskSerializer(task, many=False)
     return Response(serializer.data)
+
+
+@api_view(['POST'])
+def taskAdd(request):
+    serializer = TaskSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['POST'])
+def updateTask(request, pk):
+    task = Task.objects.get(id=pk)
+    serializer = TaskSerializer(instance=task, data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+
+    return Response(serializer.data)
+
+
+@api_view(['DELETE'])
+def deleteTask(request, pk):
+    task = Task.objects.get(id=pk)
+    task.delete()
+
+    return Response("Task deleted successfully")
